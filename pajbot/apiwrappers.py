2167d1068288ad482610caa2758b5a591f1c3d87
Rasmus Karlsson <pajlada@bithack.se>
2016-11-14 14:37:50 +0000

    fix followage

--- pajbot/apiwrappers.py
+++ pajbot/apiwrappers.py
@@ -233,7 +233,8 @@ class TwitchAPI(APIBase):
     def parse_datetime(datetime_str):
         """Parses date strings in the format of 2015-09-11T23:01:11+00:00
         to a naive datetime object."""
-        return datetime.datetime.strptime(datetime_str[:-6], '%Y-%m-%dT%H:%M:%S')
+        trimmed_str = datetime_str[:19]
+        return datetime.datetime.strptime(trimmed_str, '%Y-%m-%dT%H:%M:%S')
 
     def get_subscribers(self, streamer, limit=25, offset=0, attempt=0):
         """Returns a list of subscribers within the limit+offset range.
